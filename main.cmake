b01a6a6119642462971cba53f76f0c091b5ec045
papieros <robertrugala@gmail.com>
2026-04-19 05:08:11 +0200

    cock

--- main.cmake
+++ main.cmake
@@ -1,33 +1,26 @@
-function(inline_bash script output)
+#!/usr/bin/cmake -P
+
+function(bash script output)
     execute_process(
-            COMMAND bash -c "${script}"
-            OUTPUT_VARIABLE _output
-        )
+        COMMAND bash -c "${script}"
+        OUTPUT_VARIABLE _output
+    )
     set(${output} ${_output} PARENT_SCOPE)
+
 endfunction()
 
-function(read_line var)
-    if(NOT DEFINED var)
-        execute_process(COMMAND ./read_line.sh)
-    else()
-        execute_process(COMMAND ./read_line.sh
-            OUTPUT_VARIABLE _var
-        )
-        set(${var} ${_var} PARENT_SCOPE)
-    endif()
+function(read_line line)
+    bash("\
+            read line;\
+            echo -n $line\
+        "
+        output
+    )
+    set(${line} ${output} PARENT_SCOPE)
 endfunction()
 
-message("Macieju, jak ci na imie?: ")
+message("What is your name?:")
 
-# read_line(line)
+read_line(line)
 
-set(src
-    "echo dupa"
-    "echo -n chuj"
-)
-
-inline_bash(${src} chuj)
-message("inline_bash: '${chuj}'")
-
-# message("Twoje imie to '${line}'")
-
+message("Your name is '${line}'")
