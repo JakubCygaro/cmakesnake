45579eab87f78275067e4728ed996611a0988a81
papieros <robertrugala@gmail.com>
2026-04-20 02:39:55 +0200

    head position wrapping

--- main.cmake
+++ main.cmake
@@ -1,5 +1,6 @@
 #!/usr/bin/cmake -P
 
+cmake_minimum_required(VERSION 3.80)
 include(extern.cmake)
 
 set(GRID_SIZE 8)
@@ -9,6 +10,15 @@ set(SNAKE 4 4)
 set(DIRECTION "RIGHT")
 
 list(LENGTH SNAKE SNAKE_LEN)
+
+function(wrap_pos val min max out)
+    if(${val} LESS ${min})
+        set(${out} ${max} PARENT_SCOPE)
+    elseif(${val} GREATER ${max})
+        set(${out} ${min} PARENT_SCOPE)
+    endif()
+
+endfunction()
 
 macro(move_snake)
     list(GET SNAKE 0 head_x)
@@ -22,10 +32,16 @@ macro(move_snake)
     elseif(DIRECTION STREQUAL "UP")
         math(EXPR head_y "${head_y} + 1")
     endif()
+
+    set(x ${head_x})
+    set(y ${head_y})
+    wrap_pos(head_x 0 7 x)
+    wrap_pos(head_y 0 7 y)
+
     list(POP_BACK SNAKE) # remove last y
     list(POP_BACK SNAKE) # remove last x
-    list(PREPEND SNAKE ${head_y})
-    list(PREPEND SNAKE ${head_x})
+    list(PREPEND SNAKE ${y})
+    list(PREPEND SNAKE ${x})
 
 endmacro()
 
